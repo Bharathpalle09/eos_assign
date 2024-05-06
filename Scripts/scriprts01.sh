0f5f533ba652c9eeb5b4a2e4769038ca5e2d8aaa
Bharath-1909 <bharath.palle09@gmail.com>
2024-05-06 18:59:45 +0530

    assign 2

---
+++ Scripts/scriprts01.sh
@@ -0,0 +1,4 @@
+echo -n -e "Hello world\n"
+echo "this is my first program"
+echo -n "This is a "
+echo "single line"
---
+++ Scripts/scriprts02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This is comment
+#shebang line 
+# - this is used for identification of shell for which this script is written
+# - by looking at this line ,OS is gng to run the corresponding shell
+echo -n  "Current working directory : "
+pwd  
+
+echo -n "today's date :"
+date
+echo -n "current month calender :"
+cal
+
+echo  -n "Name of OS: "
+uname
+
